518389a27227f3296a3b15123fd4c27a948d1310
pol-nqn <71024886+pol-nqn@users.noreply.github.com>
2023-10-03 10:39:48 -0300

    probando conexion

--- PFGrupo18-Resto/src/AccesoDatos/Conexion.java
+++ PFGrupo18-Resto/src/AccesoDatos/Conexion.java
@@ -1,13 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package AccesoDatos;
 
-/**
- *
- * @author mariela
- */
+import org.mariadb.jdbc.Connection;
+import java.sql.*;
+import javax.swing.JOptionPane;
+
 public class Conexion {
-    
+
+    private static final String URL = "jdbc:mariadb://localhost:3306/";
+    private static final String DB = "pf-resto-grupo18";
+    private static final String USUARIO = "root";
+    private static final String PASSWORD = "";
+    private static Connection conn;
+
+    private Conexion() {
+    }
+
+    public static Connection conectar() {
+        if (conn == null) {
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+                conn = (Connection) DriverManager.getConnection(URL + DB + "?useLegacyDatetimeCode=false&serverTimezone=UTC"
+                        + "&user=" + USUARIO + "&password=" + PASSWORD);
+
+            } catch (ClassNotFoundException ex) {
+                JOptionPane.showMessageDialog(null, "Error al cargar los drivers " + ex.getMessage());
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, "Error al conectarse a la base de datos " + ex.getMessage());
+            }
+        }
+        return conn;
+    }
 }
--- PFGrupo18-Resto/src/pfgrupo18/resto/PFGrupo18Resto.java
+++ PFGrupo18-Resto/src/pfgrupo18/resto/PFGrupo18Resto.java
@@ -1,20 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package pfgrupo18.resto;
 
-/**
- *
- * @author nstut
- */
+import AccesoDatos.Conexion;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class PFGrupo18Resto {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        Connection conn = Conexion.conectar();
+
+        if (conn != null) {
+            System.out.println("Anda la conexion");
+
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("no funca");
+        }
     }
-    
+
 }
